ced47c2f2bcca2bfa8aafe93e7ab31b977fdfbf4
Galina798 <galina7778@bk.ru>
2025-08-28 01:37:00 +0700

    добавлено получение изображений по тегу и без тега

--- Cataas.py
+++ Cataas.py
@@ -12,14 +12,16 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
-        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
+        img.thumbnail((400, 300), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
         return None
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
+    img = load_image(url_tag)
 
     if img:
         new_window = Toplevel()
@@ -38,6 +40,12 @@ def exit():
 window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
+
+tag_entry = Entry()
+tag_entry.pack()
+
+load_button = Button(text = "Загрузить по тегу", command = open_new_window)
+load_button.pack()
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
