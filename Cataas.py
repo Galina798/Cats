eb43f68d4cd2c8e95123fd474ffa2edfe899169d
Galina798 <galina7778@bk.ru>
2025-08-27 18:23:21 +0700

    создано окно в tkinter

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,26 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+
+def load_image():
+
+
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+label = Label().pack()
+
+url = "https://cataas.com/cat"
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
+
+
